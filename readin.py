1e82a1c940cec171aea9378a55e82297c54e3a03
Sitian Qian <stqian@pku.edu.cn>
2020-11-25 20:51:48 +0800

    modify readin

--- readin.py
+++ readin.py
@@ -14,11 +14,12 @@ def readinXsec(path):
     XsecErrorBars = []
     with open(path+"/crossx.html") as f:
         lines = f.readlines()
-        templateStr = re.compile(r'        <td rowspan=1><center><a href="./HTML/run_(\d+)/results.html"> ((\d+(\.\d+)*)(e-\d+)*) <font face=symbol>&#177;</font> ((\d+(\.\d+)*)(e-\d+)*) </a>  </center></td>')
+        templateStr = re.compile(r'        <td rowspan=1><center><a href="./HTML/run_(\d+)/results.html"> ((\d+(\.\d+)*)(e(-|\+)\d+)*) <font face=symbol>&#177;</font> ((\d+(\.\d+)*)(e(-|\+)\d+)*) </a>  </center></td>')
         for line in lines:
             if templateStr.match(line):
                 print(line)
                 run,Xsec,XsecErrorBar = readinLine(line)
                 Xseces.append(Xsec)
                 XsecErrorBars.append(XsecErrorBar)
+    print("readin Xsec number:",len(Xseces))
     return np.asarray(Xseces),np.asarray(XsecErrorBars)
